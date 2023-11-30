f479384b1e2b95948e7940cf8c01545ced6e77a4
Vinicus <viniciushacrinha1@gmail.com>
2023-11-29 21:08:22 -0300

    pokeList

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,19 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonTypesToHTML(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 
 function convertPolemonToHtml(pokemon){
-    return `<li class="pokemon">
-    <span class="number">#001</span>
+    return `<li class="pokemon ${pokemon.type}">
+    <span class="number">${pokemon.number}</span>
     <span class="name">${pokemon.name}</span>
 
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
         </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            <img src="${pokemon.img}" alt="${pokemon.name}">
         
     </div>
     
